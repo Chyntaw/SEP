d0a601a0592efaf9e9d4fa937f680a9675b50d98
Florian Heinen <florian.heinen@stud.uni-due.de>
2022-10-21 11:50:01 +0200

    Spring Boot init

---
+++ sep/src/main/java/com/gruppe_f/sep/SepGruppeFApplication.java
@@ -0,0 +1,13 @@
+package com.gruppe_f.sep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepGruppeFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepGruppeFApplication.class, args);
+	}
+
+}
