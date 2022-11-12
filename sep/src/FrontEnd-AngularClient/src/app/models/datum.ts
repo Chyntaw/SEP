8630efb6c799291e1c2c9c54f1eb895744efe42a
Can Kalafat <can.kalafat@stud.uni-due.de>
2022-11-12 16:46:20 +0100

    DateController Backend
    datum.ts/datum.spec.ts erstellt

---
+++ sep/src/FrontEnd-AngularClient/src/app/models/datum.ts
@@ -0,0 +1,5 @@
+export class Datum {
+
+  datum!:Date;
+
+}
--- sep/src/main/java/com/gruppe_f/sep/date/DateController.java
+++ sep/src/main/java/com/gruppe_f/sep/date/DateController.java
@@ -1,4 +1,29 @@
 package com.gruppe_f.sep.date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class DateController {
+
+    private final DateService service;
+
+    @Autowired
+    public DateController(DateService service) {
+        this.service = service;
+    }
+
+
+    @PostMapping("/user/systemdatum")
+    public ResponseEntity<?> datumAendern(@RequestBody Date date){
+        Date newDate = service.changeDate(date);
+
+        return new ResponseEntity<>(newDate, HttpStatus.OK);
+    }
 }
