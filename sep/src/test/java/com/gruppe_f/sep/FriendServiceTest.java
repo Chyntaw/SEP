193ea877461efb38aa51f5459ea0f1f3be386249
Danial <Supportdanial.imani-shakibaei@stud.uni-due.de>
2022-12-08 12:19:21 +0100

    Modultest - "FriendServiceTest"

---
+++ sep/src/test/java/com/gruppe_f/sep/FriendServiceTest.java
@@ -0,0 +1,35 @@
+package com.gruppe_f.sep;
+
+import com.gruppe_f.sep.entities.friends.Friend;
+import com.gruppe_f.sep.entities.friends.FriendRepository;
+import com.gruppe_f.sep.entities.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class FriendServiceTest {
+
+
+    @Test
+    void contextLoads(){
+    }
+
+    @Autowired
+    FriendRepository friendRepository;
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Test
+    @DisplayName("Test should pass, if FriendRequest is sended")
+    void shouldSendFriendReqeust(){
+        Friend friend = new Friend(userRepository.findUserById(1L), userRepository.findUserById(2L), true);
+        friendRepository.save(friend);
+        Assertions.assertNotNull(friendRepository.findByFirstUserAndSecondUser(userRepository.findUserById(1L), userRepository.findUserById(2L)));
+    }
+}
