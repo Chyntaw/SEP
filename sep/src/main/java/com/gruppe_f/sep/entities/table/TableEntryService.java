77d68d458976557718f1273eccdf2c3c59165472
Max <max.eschenbach@stud.uni-due.de>
2023-01-26 00:32:29 +0100

    minor improvements

--- sep/src/main/java/com/gruppe_f/sep/entities/table/TableEntryService.java
+++ sep/src/main/java/com/gruppe_f/sep/entities/table/TableEntryService.java
@@ -1,9 +1,8 @@
 package com.gruppe_f.sep.entities.table;
-
 import com.gruppe_f.sep.entities.bets.Bets;
 import com.gruppe_f.sep.entities.leagueData.LeagueData;
-
 import java.util.*;
+
 
 public class TableEntryService {
 
@@ -11,82 +10,50 @@ public class TableEntryService {
     //If param E not instanceof Bets or LeagueData -> IllegalArgumentException
     public static <E> List<TableEntry> calculateTable(List<E> list) {
 
-        if(list.isEmpty()) throw new NullPointerException("List is Empty");
-
-        List<TableEntry> table = new LinkedList<>();
+        Map<String, TableEntry> tableEntries = new HashMap<>();
 
         for(E element:list) {
 
-            String player1;
-            String player2;
+            String team1;
+            String team2;
             String result;
             List<TableEntry> newEntries;
-            TableEntry player1Entry;
-            TableEntry player2Entry;
 
             //Check if elements of List are instances of "Bets" and
             //cast element to Bets -> currBet
             if(element instanceof Bets currBet) {
 
-                player1 = currBet.getLeagueData().getPlayer1();
-                player2 = currBet.getLeagueData().getPlayer2();
+                team1 = currBet.getLeagueData().getPlayer1();
+                team2 = currBet.getLeagueData().getPlayer2();
                 result = currBet.getBets();
 
-                //Get 2 new TableEntries for this Game, since both Players
-                //Points need to be updated
-                newEntries = newTableEntry(player1, player2, result);
+                //Get 2 new TableEntries for this Game, since both Teams points need to be updated
+                newEntries = newTableEntry(team1, team2, result);
 
-                player1Entry = newEntries.get(0);
-                player2Entry = newEntries.get(1);
-
-            //Check if elements of List are of Type LeagueData
+            //Check if elements of List are of Type LeagueData, otherwise identical to above
             } else if(element instanceof LeagueData currData) {
 
-                player1 = currData.getPlayer1();
-                player2 = currData.getPlayer2();
+                team1 = currData.getPlayer1();
+                team2 = currData.getPlayer2();
                 result = currData.getResult();
 
-                newEntries = newTableEntry(player1, player2, result);
-
-                player1Entry = newEntries.get(0);
-                player2Entry = newEntries.get(1);
+                newEntries = newTableEntry(team1, team2, result);
 
             //If not of Type Bet or LeagueData, throw Exception
             } else {
                 throw new IllegalArgumentException("List must contain instances of 'Bets' or 'LeagueData'");
             }
 
-            //Update Table with new Entries
-            if(!table.isEmpty()) {
-                boolean p1changed = false;
-                boolean p2changed = false;
-                for (TableEntry entry : table) {
-                    //Check if curr Entry is for Team 1
-                    if (entry.getTeam().equals(player1Entry.getTeam())) {
-                        //Update existing Table - Entry
-                        combineEntries(entry, player1Entry);
-                        p1changed = true;
-                    }
-                    if (entry.getTeam().equals(player2Entry.getTeam())) {
-
-                        combineEntries(entry, player2Entry);
-                        p2changed = true;
-
-                    }
-                }
-                //If not changed before, Players not yet in List -> have to be added here
-                if(!p1changed) table.add(player1Entry);
-                if(!p2changed) table.add(player2Entry);
-            //If Table empty, add Both new Entries
-            } else{
-                table.add(player1Entry);
-                table.add(player2Entry);
-            }
+            //If Team not in Table create entry; otherwise update existing entries in Map using newEntries from above.
+            tableEntries.put(team1, combineEntries(tableEntries.get(team1), newEntries.get(0)));
+            tableEntries.put(team2, combineEntries(tableEntries.get(team2), newEntries.get(1)));
         }
-        //Calculate TorDifference
-        for(TableEntry entry : table) entry.setDiff(entry.getToreGeschossen()- entry.getToreKassiert());
 
-        //Comparators, so that list can be Sorted by Points first, after that sorted by Goal Difference
+        //Convert Map.values to List, to sort and return
+        List<TableEntry> table = new LinkedList<>(tableEntries.values());
+
+        //Comparators to sort by Points first and GoalDifference second
+        //.reversed(), since Table need to be sorted max -> min
         Comparator<TableEntry> punkteComparator = Comparator.comparing(TableEntry::getPunkte).reversed();
         Comparator<TableEntry> torDiffComparator = Comparator.comparing(TableEntry::getDiff).reversed();
         Comparator<TableEntry> doppelComparator = punkteComparator.thenComparing(torDiffComparator);
@@ -94,51 +61,50 @@ public class TableEntryService {
         Collections.sort(table, doppelComparator);
 
         return table;
-
     }
 
-    //Generates a List of TableEntries for 1 Game.
-    //Requires Player1, Player2 and Result of game.
-    public static List<TableEntry> newTableEntry (String player1, String player2, String result) {
+    //Generates a List of 2 TableEntries for 1 Game.
+    public static List<TableEntry> newTableEntry (String team1, String team2, String result) {
 
         int goalsPlayer1 = Integer.parseInt(result.split("-")[0]);
         int goalsPlayer2 = Integer.parseInt(result.split("-")[1]);
 
-        TableEntry entryPlayer1 = new TableEntry(player1, 1, goalsPlayer1, goalsPlayer2);
-        TableEntry entryPlayer2 = new TableEntry(player2, 1, goalsPlayer2, goalsPlayer1);
+        TableEntry entryTeam1 = new TableEntry(team1, 1, goalsPlayer1, goalsPlayer2);
+        TableEntry entryTeam2 = new TableEntry(team2, 1, goalsPlayer2, goalsPlayer1);
 
         //Check for Unentschieden
         if(goalsPlayer1 == goalsPlayer2) {
 
-            entryPlayer1.setUnentschieden(1);
-            entryPlayer1.setPunkte(entryPlayer1.getUnentschiedenPunkte());
+            entryTeam1.setUnentschieden(1);
+            entryTeam1.setPunkte(entryTeam1.getUnentschiedenPunkte());
 
-            entryPlayer2.setUnentschieden(1);
-            entryPlayer2.setPunkte(entryPlayer2.getUnentschiedenPunkte());
-        //Check if Player1 is Winner
+            entryTeam2.setUnentschieden(1);
+            entryTeam2.setPunkte(entryTeam2.getUnentschiedenPunkte());
+        //Check if Team1 is Winner
         } else if (goalsPlayer1 > goalsPlayer2) {
-            entryPlayer1.setSiege(1);
-            entryPlayer1.setPunkte(entryPlayer1.getSiegPunkte());
+            entryTeam1.setSiege(1);
+            entryTeam1.setPunkte(entryTeam1.getSiegPunkte());
 
-            entryPlayer2.setVerloren(1);
-        //If not Unentschieden, and Player1 is not Winner, Player2 must be winner
+            entryTeam2.setVerloren(1);
+        //If not Unentschieden, and Team1 is not Winner, Team2 must be winner
         } else {
-            entryPlayer2.setSiege(1);
-            entryPlayer2.setPunkte(entryPlayer2.getSiegPunkte());
+            entryTeam2.setSiege(1);
+            entryTeam2.setPunkte(entryTeam2.getSiegPunkte());
 
-            entryPlayer1.setVerloren(1);
+            entryTeam1.setVerloren(1);
         }
-        //Return both new Entries
+        //Return both new entries with Team1 at index 0, Team2 at index 1
         List<TableEntry> returnList = new LinkedList<>();
-        returnList.add(entryPlayer1);
-        returnList.add(entryPlayer2);
+        returnList.add(entryTeam1);
+        returnList.add(entryTeam2);
 
         return returnList;
     }
 
     //Combine Entries, so that Reference to param1 is not lost.
-    //param2 is consumed and lost.
     public static TableEntry combineEntries(TableEntry entry, TableEntry consumedEntry) {
+
+        if(entry == null) return consumedEntry;
 
         entry.setToreGeschossen(entry.getToreGeschossen() + consumedEntry.getToreGeschossen());
         entry.setToreKassiert(entry.getToreKassiert() + consumedEntry.getToreKassiert());
@@ -147,6 +113,8 @@ public class TableEntryService {
         entry.setSiege(entry.getSiege() + consumedEntry.getSiege());
         entry.setUnentschieden(entry.getUnentschieden() + consumedEntry.getUnentschieden());
         entry.setVerloren(entry.getVerloren() + consumedEntry.getVerloren());
+
+        entry.setDiff(entry.getToreGeschossen() - entry.getToreKassiert());
 
         return entry;
     }
